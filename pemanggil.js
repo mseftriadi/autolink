c8cd82f137d4055ddbde96252488b8b594fa16b8
mseftriadi <35690745+mseftriadi@users.noreply.github.com>
2018-01-22 20:45:37 +0700

    Update pemanggil.js

--- pemanggil.js
+++ pemanggil.js
@@ -25,7 +25,7 @@ if(x=="1"){xxx="/2018/01/bpjs-kesehatan-semarang-held-evaluation.html"}
 if(x=="2"){xxx="/2018/01/president-instructs-11-leaders-to.html"}
 if(x=="3"){xxx="/2018/01/surakarta-city-towards-universal-health.html"}
 if(x=="4"){xxx="/2018/01/mayor-of-medan-committed-to-universal.html"}
-if(x=="5"){xxx="/2018/01/lumajang-district-signed-universal.html"}a_to_ve[i].href="https://safeshorlink.blogspot.co.id/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="5"){xxx="/2018/01/lumajang-district-signed-universal.html"}a_to_ve[i].href="https://safeshorlink.blogspot.com/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
